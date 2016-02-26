a7ba08663d01013c659c391a85c2db853e76a0b1
ahmed-shehata <ahmed.shehata@espace.com.eg>
2016-02-26 02:11:42 +0200

    Initial Commit

---
+++ MenuBar-Ping/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MenuBar-Ping
+//
+//  Created by Ahmed Shehata on 2/26/16.
+//  Copyright © 2016 Bitmonkey Games. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
